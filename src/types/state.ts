5f51b1e307472fc7d3ed8001fd983a3791567b19
rajagiri484 <rajagiri484@gmail.com>
2025-02-05 14:48:29 +0530

    comments removal

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,13 +1,12 @@
 // Infer the `RootState` and `AppDispatch` types from the store itself
-import {store} from '../store';
+import { store } from "../store";
 
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
 export enum RequestStatus {
-  IDLE = 'idle',
-  PENDING = 'pending',
-  SUCCESSFULL = 'successfull',
-  FAILED = 'failed',
+  IDLE = "idle",
+  PENDING = "pending",
+  SUCCESSFULL = "successfull",
+  FAILED = "failed",
 }
